a59d93d77b37617712ad847daa99295967a71fb6
RobMeSa <rmezas1900@alumno.ipn.mx>
2023-03-17 12:31:58 -0600

    labpython1

---
+++ labpython1.py
@@ -0,0 +1,30 @@
+''' ESTE ES UN SUPERCOMENTARIO
+    DE INICIO A NUESTRO RESUMEN
+'''
+#======================
+# Operaciones básicas
+#=====================
+print (2+3)
+print (2*3)
+print (2/3)
+print (2**10)
+print (2**.5)
+print (10%2)
+print (10%.1)
+
+#=========================================
+# Para saber el tipo de objeto se usa type
+#=========================================
+t=0
+print(type(t)) #entero
+t=3.6
+print(type(t)) #real (flotante)
+t=True
+print(type(t)) #booleano (bool)
+
+#==========================
+# Mensajes a pantalla
+#==========================
+print("Este es un comando de python. ", "Este es otro enunciado.", t)
+print('id: ',1)
+print('First Name: ', 'Steve')
