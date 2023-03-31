23c8ba7cd8870d6359ac5f58946f1ad5f5d70a92
RoBMeSa <lriveraz1700@alumno.ipn.mx>
2023-03-31 12:30:57 -0600

    labpython1.py

--- labpython1.py
+++ labpython1.py
@@ -150,3 +150,152 @@ print(parrafo)
 # La funcion len() obtiene el tamaño del string
 #================================================
 n = len(parrafo)
+print(n)
+
+#===================================================================
+#   Las letras en un string ocupan lugar como en un arreglo 
+#   (también de atras para adelante comenzando en -1 el último)
+#===================================================================
+palabra = "hola"
+print(palabra[0])
+print(palabra[-4])
+
+#===============================================
+# Conjunto en Python
+#===============================================
+even_nums = {2,4,6,8,10} #Conjunto de numeros pares
+print(even_nums)
+
+# El bool no es parte del conjunto
+emp = {1,'Steve',10.5,True} #Conjunto de diferentes objetos
+print(emp)
+
+nums = {1,2,2,3,4,4,5,5}
+print(nums)
+
+#================================================
+#  Convertir secuencia a conjunto 
+# No lo genera en orden
+#================================================
+s = set('hello')
+print(s)
+s = set((1,2,3,4,5)) # Tupla a conjunto
+print(s)
+
+s.add(100)
+print(s)
+
+s.update(nums)
+print(s)
+
+s.remove(100)
+print(s)
+
+s1={1,2,3,4,5}
+s2={4,5,6,7,8}
+
+su = s1|s2 #Union
+print(su)
+
+si = s1&s2 #Interseccion
+print(si)
+
+sr = s1 - s2  #Diferencia de conjuntos
+print(sr)
+
+sp = s2-s1
+print(sp)
+
+ss = s1^s2 #Diferencia simétrica
+print(ss)
+
+#============================================
+# Uso de diccionarios
+#============================================
+capitals = {"USA":"Washington D.C", "France":"Paris","India":"New Dehli"}
+print(capitals)
+
+#============================================
+# Llave valor
+#============================================
+
+# Diccionario valor
+d={}
+
+#Llave entera, valor string
+numNames={1:"One", 2:"Two", 3:"Three"}
+
+#Llave real, valor string
+numNames={1:"One and half", 2.5: "Two and half", 3.5:"Three and half"}
+
+#Llave tupla, valor string
+items={("Parker","Reynolds","Camlin"):"pen",("LG","Whirpool","Samsung"):"Refrigerator"}
+
+#Llave string, valor int
+romanNums = {'I':1,'II':2,'III':3,'IV':4, 'V':5}
+print(romanNums)
+print(romanNums["I"])
+
+print(capitals.get("India"))
+print(capitals.get("india"))
+
+#Reportar llave y valor
+capitals["Mexico"] = "CDMX"
+print(capitals)
+
+#Borrar dato del diccionario
+del capitals["Mexico"]
+print(capitals)
+
+#Borrar todo el diccionario
+del capitals
+
+#Reportar llaves
+print(romanNums.keys())
+
+#Reportar valores 
+print(romanNums.values())
+
+#Juicio de llave (está o no está la llave en el diccionario)
+print("I" in romanNums)
+print("X" in romanNums)
+print("XX" not in romanNums)
+
+#===================================================
+#    Listas
+#    Las listas pueden ser de objetos diferentes
+#===================================================
+
+miprimeralista = [] #Lista vacia
+print(miprimeralista)
+
+#===================================================
+#    Llenado de lista
+#===================================================
+miprimeralista = [1, "Javier", 1.34,"Bosco", "Angel", "Abigail", True]
+print(miprimeralista)
+
+#===================================================
+#   list: hacer una lista
+#   range(i,j): secuencia de i hasta j-1
+#===================================================
+nums = list(range(1,61))
+
+for i in nums:
+    print(i)
+
+#===================================================
+#  Incluir nuevos elementos en la lista
+#===================================================
+nums.append(61)
+nums.append(62)
+nums.append(63)
+print(nums)
+
+#==================================================
+# Quitar elementos de la lista
+#==================================================
+nums.remove(61)
+print(nums)
+
+
