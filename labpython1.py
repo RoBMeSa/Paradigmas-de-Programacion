537ef0c247575fc1b4f6e8bcd052381a7ed65e1f
RoBMeSa <lriveraz1700@alumno.ipn.mx>
2023-04-21 13:31:12 -0600

    labpython1.py

--- labpython1.py
+++ labpython1.py
@@ -613,5 +613,98 @@ if flag == True:
 print(s)
 
 
-    
+#========================================
+# Programacion orientada a objetos
+#========================================
+
+#========================================
+# Una clase para un objeto vacio
+# No está tan vacio, necesita la 
+# Palabra pass = pasar 
+#========================================
+class ObjetoVacio:
+    pass
+#==========================================
+# Nada es un objeto vacio
+#==========================================
+
+nada = ObjetoVacio()
+print(type(nada))
+
+#========================================
+# La clase llanta
+#========================================
+class Llanta:
+    #====================================
+    # Variable cuenta es de toda la clase
+    #====================================
+    cuenta = 0
+    #====================================
+    # Funcion constructora de identidad
+    # __init__ es una funcion reservada
+    # comienza por uno mismo: self
+    # pero puede ser otro nombre (mi)
+    # parametros de entrada  = default
+    #====================================
+    def __init__(mi, radio = 50, ancho = 30, presion = 1.5):
+        # variable de la estructura completa llanta
+        Llanta.cuenta += 1
+        # Variables exclusivas de cada objeto
+        mi.radio = radio
+        mi.ancho = ancho
+        mi.presion = presion
+
+#==============================
+# Objetos instanciados
+#==============================
+llanta1 = Llanta(50,30,1.5)
+llanta2 = Llanta(presión=1.2)
+llanta3 = Llanta()
+llanta4 = Llanta(40,30,1.6)
+
+#==========================================
+# Objeto que contiene otros objetos
+#==========================================
+class Coche:
+    def __init__(mi, ll1,ll2,ll3,ll4):
+        mi.llanta1 = ll1
+        mi.llanta2 = ll2
+        mi.llanta3 = ll3
+        mi.llanta4 = ll4
+micoche = Coche(llanta1, llanta2, llanta3, llanta4)
+
+print("Total de llantas: ", Llanta.cuenta) #Variable global de la clase
+print("Presion de la llanta 4 : ", llanta4.presion)
+print("Radio de la llanta 3", llanta3.radio)
+print("Presion de la llanta 1 de mi coche= ", micoche.llanta1.presion)
+
+#==================
+# Encapsulamiento
+#==================
+class Estudiante:
+    def __init__(mi):
+        mi.__nombre = ''
+    def ponerme_nombre(mi, nombre):
+        print("Se llamó a ponerme_nombre")
+        mi.__nombre = nombre
+    def obtener_nombre(mi):
+        print("se llamó a obtener_nombre")
+        return mi.__nombre
+    nombre=property(obtener_nombre, ponerme_nombre)
+
+#==========================================
+# Crear objeto estudiante sin nombre
+#==========================================
+estudiante = Estudiante()
+
+#=========================================================
+# Ponerle nombre usando la propiedad nombre y el método ponerme_nombre
+# (Sin tener que llamar explicitamente a la funcion)
+#=========================================================
+
+estudiante.nombre = "Diego"
+#=========================================================
+# Obtener nombre con el metodo obtener_nombre
+# __nombre es una variable encapsulada, no visible desde afuera
+#==========================================================
 
