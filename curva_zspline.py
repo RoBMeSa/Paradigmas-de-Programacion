d237aaa5f6a2f64067678fb0845ba4bf6904359d
RoBMeSa <rmezas1900@alumno.ipn.mx>
2023-04-28 13:23:51 -0600

    curva_zspline

---
+++ curva_zspline.py
@@ -0,0 +1,12 @@
+import numpy as np
+from curvaZpline import Curva, zspline
+import matplotlib.pyplot as plt
+import math
+
+#=============================================
+# Curvas Z-splines en n dimensiones
+#=============================================
+# Julian Sagredo ESFM 2023
+#=============================================
+
+
